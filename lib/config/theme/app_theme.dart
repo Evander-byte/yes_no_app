7530f39a9d82bf85b2e3f6bb671d62044ce5fb72
Evander-byte <emondragon571@gmail.com>
2024-09-24 12:32:12 -0600

    Initial Commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF8454D1);
+
+const List<Color> _colorTheme = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink
+];
+
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData(
+      colorSchemeSeed: _colorTheme[4],
+    );
+  }
+}
