1d12b59c9912d914db89fc14601585355b0091b5
Evander-byte <emondragon571@gmail.com>
2024-09-25 12:01:21 -0600

    Bubble message

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _AppBar(),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: false,
+      leading: const Padding(
+        padding: EdgeInsets.all(4.0),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+              'http://cdn.pixabay.com/photo/2021/06/22/14/55/katara-6356390_1280.jpg'),
+        ),
+      ),
+      title: const Text('Pecorina 😍'),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return const MyMessageBubble();
+                },
+              ),
+            ),
+            Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
