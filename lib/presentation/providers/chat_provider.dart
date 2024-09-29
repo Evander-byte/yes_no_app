c133730c5d496c476275bca22b9931e8a6474394
Evander-byte <emondragon571@gmail.com>
2024-09-29 15:56:56 -0600

    Message from input and scroll to last message

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,10 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  final ScrollController chatScrollController = ScrollController();
+  List<Message> messageList = [
     Message(text: 'Hola Amor', fromWho: FromWho.me),
     Message(text: 'Estas con lula?', fromWho: FromWho.me)
   ];
 
-  Future<void> sendMessage(String text) async {}
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
+  }
 }
